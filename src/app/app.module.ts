15e4b7df37a704afecaa3fd6d9803cbaac487aba
clara pecot <clara.pecot44@gmail.com>
2024-05-15 16:57:16 +0200

    new component 'client-account' created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
-import { ClientListComponent } from './pages/client-list/client-list.component';  
+import { ClientListComponent } from './pages/client-list/client-list.component';
 import { AppRoutesModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { ClientCreateComponent } from './pages/client-create/client-create.component';
 import { ClientDetailsComponent } from './pages/client-details/client-details.component';
 import { ClientEditComponent } from './pages/client-edit/client-edit.component';
+import { ClientAccountComponent } from './pages/client-account/client-account.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { ClientEditComponent } from './pages/client-edit/client-edit.component';
     ClientListComponent,/* -> Declare ClientListComponent */
     ClientCreateComponent,
     ClientDetailsComponent,
-    ClientEditComponent
+    ClientEditComponent,
+    ClientAccountComponent
   ],
   imports: [
     BrowserModule,
@@ -29,5 +31,3 @@ import { ClientEditComponent } from './pages/client-edit/client-edit.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-
