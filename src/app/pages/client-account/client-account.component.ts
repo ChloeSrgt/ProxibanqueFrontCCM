219c0e42a82a386790f16e16d1c1e19ca6eb63f8
clara pecot <clara.pecot44@gmail.com>
2024-05-20 21:04:10 +0200

    raw code for test (read a client account) added + tables added

--- src/app/pages/client-account/client-account.component.ts
+++ src/app/pages/client-account/client-account.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class ClientAccountComponent {
 
+  client = {
+    firstName: 'Bakary',
+    lastName: 'Sagnat',
+    listAccount: [
+      { currentAccount: 'Current account' },
+      { savingAccount: 'Saving account' },
+      { testAccount: [
+        { numAccount: '12345' },
+        { solde: 3000 },
+        { openDate: '20/20/2023' }
+      ]
+    }
+    ]
+  };
+
 }
