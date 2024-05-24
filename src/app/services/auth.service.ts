fd0f665229251456f219c7f8cae48a0f8409ed34
Marine <marine.delanoe53@gmail.com>
2024-05-24 09:42:39 +0200

    add authentification on login page

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,14 +4,28 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
-  private validCredentials = {
+/*   private validCredentials = {
     username: 'admin',
     password: 'admin123'
-  };
+  }; */
 
   constructor() { }
 
-  login(username: string, password: string): boolean {
+ /*  login(username: string, password: string): boolean {
     return username === this.validCredentials.username && password === this.validCredentials.password;
+  } */
+
+  testUser(username:string,password:string){
+    if (username =='manager' && password =='manager') {
+    return 'manager'
+  } else {
+    if (username == 'advisor' && password == 'advisor'){
+      return 'advisor'
+    }
+  } return 'NotAllowed'
+    
   }
-}
+    
+  }
+
+
