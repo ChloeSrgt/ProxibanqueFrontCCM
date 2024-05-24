a46eac02ae71af558253c24aa71823d501e18257
ChloeSrgt <chloe.sirgent@gmail.com>
2024-05-24 11:27:39 +0200

    update

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -10,16 +10,31 @@ import { Router } from '@angular/router';
   standalone: false,
 })
 export class LoginComponent {
+
+
   username = '';
   password = '';
 
   constructor(private authService: AuthService, private router: Router) {}
 
-  onLogin(): void {
+/*   onLogin(): void {
     if (this.authService.login(this.username, this.password)) {
-      this.router.navigate(['/client-list']);  
+      this.router.navigate(['/client-list']);
     } else {
       alert('Invalid credentials!');
     }
+  } */
+
+logMe(username : HTMLInputElement, password:HTMLInputElement) {
+  let role = this.authService.testUser(username.value,password.value);
+  if(role == 'manager'){
+    this.router.navigate(['advisors-list'])
+  }else {
+    if(role == 'advisor'){
+      this.router.navigate(['client-list'])
+    } else window.alert("You're not allowed to log in")
   }
+
+}
+
 }
