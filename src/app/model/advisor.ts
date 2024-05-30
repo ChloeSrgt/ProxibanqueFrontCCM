a3a55a120d2a97b40ebc3408d15cc9086ccb1c4b
clara pecot <clara.pecot44@gmail.com>
2024-05-30 14:25:26 +0200

    some more advisors created + some clients created for each advisor

--- src/app/model/advisor.ts
+++ src/app/model/advisor.ts
@@ -1,5 +1,12 @@
+import { Client } from "./client";
+
 export class Advisor {
   lastName: string = '';
   firstName: string = '';
   email: string = '';
+  clients: Client[] = [];
+
+  constructor(data: Partial<Advisor> = {}) {
+    Object.assign(this, data);
+  }
 }
--- src/app/pages/advisors-list/advisors-list.component.ts
+++ src/app/pages/advisors-list/advisors-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Advisor } from '../../model/advisor';
 import { ActivatedRoute } from '@angular/router';
 import { ClientService } from '../../services/client.service';
+import { Client } from '../../model/client';
 
 @Component({
   selector: 'app-advisors-list',
@@ -15,17 +16,60 @@ export class AdvisorsListComponent implements OnInit {
     {
       lastName: 'Ramiandrisoa',
       firstName: 'Agnès',
-      email: 'aramiandrisoa@proxibanque.fr'
+      email: 'aramiandrisoa@proxibanque.fr',
+      clients: [
+        new Client({ firstName: 'Marina', lastName: 'Sinai' }),
+        new Client({ firstName: 'Christophe', lastName: 'Galtier' }),
+        new Client({ firstName: 'Grégoire', lastName: 'Bassora' }),
+        new Client({ firstName: 'Maurice', lastName: 'Arica' }),
+        new Client({ firstName: 'Josette', lastName: 'Dumont' })
+      ]
     },
     {
       lastName: 'Boureau',
       firstName: 'Audrey',
-      email: 'aboureau@proxibanque.fr'
+      email: 'aboureau@proxibanque.fr',
+      clients: [
+        new Client({ firstName: 'Didier', lastName: 'Deschamps' }),
+        new Client({ firstName: 'Cindy', lastName: 'Galliano' }),
+        new Client({ firstName: 'Laurie', lastName: 'Damiette' }),
+        new Client({ firstName: 'Paul', lastName: 'Calera' })
+      ]
     },
     {
       lastName: 'Asri',
       firstName: 'Elias',
-      email: 'easri@proxibanque.fr'
+      email: 'easri@proxibanque.fr',
+      clients: [
+        new Client({ firstName: 'Philippe', lastName: 'Lucas' }),
+        new Client({ firstName: 'Mireille', lastName: 'Matthieu' }),
+        new Client({ firstName: 'Tatiana', lastName: 'Thane' }),
+        new Client({ firstName: 'Pablo', lastName: 'Molina' })
+      ]
+    },
+    {
+      lastName: 'Durozier',
+      firstName: 'Alexane',
+      email: 'adurozier@proxibanque.fr',
+      clients: [
+        new Client({ firstName: 'Emilie', lastName: 'Jones' }),
+        new Client({ firstName: 'Peter', lastName: 'Parker' }),
+        new Client({ firstName: 'Theophil', lastName: 'Delamare' }),
+        new Client({ firstName: 'Carlos', lastName: 'Duboc' }),
+        new Client({ firstName: 'Pierre', lastName: 'Simon' }),
+        new Client({ firstName: 'Lily', lastName: 'Potter' })
+      ]
+    },
+    {
+      lastName: 'Spaak',
+      firstName: 'Marine',
+      email: 'mspaak@proxibanque.fr',
+      clients: [
+        new Client({ firstName: 'Coralie', lastName: 'Lannion' }),
+        new Client({ firstName: 'Marie', lastName: 'Erquy' }),
+        new Client({ firstName: 'Barnabé', lastName: 'Le Faou' }),
+        new Client({ firstName: 'Renée', lastName: 'Guirec' })
+      ]
     }
   ];
 
@@ -35,11 +79,22 @@ export class AdvisorsListComponent implements OnInit {
     private route: ActivatedRoute
   ) { }
 
+  sortAdvisors(): void {
+    this.advisors.sort((a, b) => a.lastName.localeCompare(b.lastName));
+  }
+
+  sortClients(): void {
+    this.advisors.forEach(advisor => {
+      advisor.clients.sort((a, b) => a.lastName.localeCompare(b.lastName));
+    });
+  }
+
   selectAdvisor(advisor: Advisor): void {
     this.selectedAdvisor = advisor;
   }
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.sortAdvisors();
+    this.sortClients();
   }
 }
