2f2f137db59f20fef707d2813567dba4221bc68b
Marine <marine.delanoe53@gmail.com>
2024-06-05 16:21:45 +0200

    add required on form and add a page with Client's info

---
+++ src/app/pages/client-info/client-info.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { ClientService } from '../../services/client.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Client } from '../../model/client';
+import { Address } from '../../model/address';
+
+@Component({
+  selector: 'app-client-info',
+  templateUrl: './client-info.component.html',
+  styleUrl: './client-info.component.css'
+})
+export class ClientInfoComponent {
+  id = this.activatedRoute.snapshot.params['id'];
+  clientInfo: any = {};
+
+  constructor( private service: ClientService, private activatedRoute: ActivatedRoute, private router: Router) {
+    this.clientInfo = new Client();
+    this.clientInfo.address = new Address();
+}
+
+ngOnInit() {
+  this.service.getClient(this.id).subscribe((data: {}) => {
+    this.clientInfo = data;
+  })
+}
+
+
+}
